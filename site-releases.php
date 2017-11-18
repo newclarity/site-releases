f4335dde4da26ee72308c46567dd4b9ab33249ea
Mike Schinkel <mike@newclarity.net>
2017-11-17 19:01:21 -0500

    Bump version to 0.1.4

--- site-releases.php
+++ site-releases.php
@@ -3,7 +3,7 @@
 Plugin Name: Site Releases
 Plugin URI: https://github.com/newclarity/site-releases
 Description: Help agencies to track site releases
-Version: 0.1.3
+Version: 0.1.4
 Author: The NewClarity Team
 Author URI: http://newclarity.net
 Text Domain: site-releases
